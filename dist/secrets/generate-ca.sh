61043d10a452aba696bc5346e28d75b00d1c7885
Bruce Ma <brucema19901024@gmail.com>
2021-06-17 15:26:45 +0800

    chore: use default CA and generate tls certificates when build
    Signed-off-by: Bruce Ma <brucema19901024@gmail.com>

---
+++ dist/secrets/generate-ca.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# generate root CA
+openssl req -nodes -new -x509 -days 3650 -keyout ca.key -out ca.crt -subj "/CN=rama"
+
+# encode root CA
+ca_bundle=$(openssl base64 -A < ca.crt)
+echo $ca_bundle > ca.bundle
