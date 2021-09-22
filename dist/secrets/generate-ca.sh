2a3e6cbdc99044e7fd256656269048d35cab61ac
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-22 15:17:01 +0800

    Delete dist directory

--- dist/secrets/generate-ca.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# generate root CA
-openssl req -nodes -new -x509 -days 3650 -keyout ca.key -out ca.crt -subj "/CN=hybridnet"
-
-# encode root CA
-ca_bundle=$(openssl base64 -A < ca.crt)
-echo "${ca_bundle}" > ca.bundle
