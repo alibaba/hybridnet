c0f562f5ed611bb1a7b4f5d7f757cd8340f18b16
Bruce Ma <brucema19901024@gmail.com>
2022-01-19 14:55:59 +0800

    fix some typo
    Signed-off-by: Bruce Ma <brucema19901024@gmail.com>

--- pkg/utils/transform/ipam.go
+++ pkg/utils/transform/ipam.go
@@ -29,7 +29,7 @@ func TransferSubnetForIPAM(in *v1.Subnet) *ipamtypes.Subnet {
 
 	return ipamtypes.NewSubnet(in.Name,
 		in.Spec.Network,
-		int32pTouint32p(in.Spec.NetID),
+		int32pToUint32p(in.Spec.NetID),
 		net.ParseIP(in.Spec.Range.Start),
 		net.ParseIP(in.Spec.Range.End),
 		net.ParseIP(in.Spec.Range.Gateway),
@@ -44,7 +44,7 @@ func TransferSubnetForIPAM(in *v1.Subnet) *ipamtypes.Subnet {
 
 func TransferNetworkForIPAM(in *v1.Network) *ipamtypes.Network {
 	return ipamtypes.NewNetwork(in.Name,
-		int32pTouint32p(in.Spec.NetID),
+		int32pToUint32p(in.Spec.NetID),
 		in.Status.LastAllocatedSubnet,
 		ipamtypes.ParseNetworkTypeFromString(string(v1.GetNetworkType(in))),
 	)
@@ -54,7 +54,7 @@ func TransferIPInstanceForIPAM(in *v1.IPInstance) *ipamtypes.IP {
 	return &ipamtypes.IP{
 		Address:      utils.StringToIPNet(in.Spec.Address.IP),
 		Gateway:      net.ParseIP(in.Spec.Address.Gateway),
-		NetID:        int32pTouint32p(in.Spec.Address.NetID),
+		NetID:        int32pToUint32p(in.Spec.Address.NetID),
 		Subnet:       in.Spec.Subnet,
 		Network:      in.Spec.Network,
 		PodName:      in.Status.PodName,
@@ -71,7 +71,7 @@ func TransferIPInstancesForIPAM(ips []*v1.IPInstance) []*ipamtypes.IP {
 	return ret
 }
 
-func int32pTouint32p(in *int32) *uint32 {
+func int32pToUint32p(in *int32) *uint32 {
 	if in == nil {
 		return nil
 	}
