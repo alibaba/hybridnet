fe78bb34233fe644f27293d297091f4104aa2dc0
hhyasdf <552483776@qq.com>
2022-03-16 18:06:51 +0800

    init helm repo

--- pkg/constants/nic.go
+++
@@ -1,8 +0,0 @@
-package constants
-
-const (
-	ContainerHostLinkPrefix = "hybr"
-	ContainerHostLinkMac    = "ee:ee:ee:ee:ee:ee"
-	VxlanLinkInfix          = ".vxlan"
-	ContainerNicName        = "eth0"
-)
