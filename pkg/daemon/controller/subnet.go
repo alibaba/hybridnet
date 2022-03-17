f1bf2dfb3e33d5a3da36fe14332a4457fdf06358
Bruce Ma <brucema19901024@gmail.com>
2022-03-17 19:21:50 +0800

    [RELEASE cherry-pick] bugfix: static ip migrating error

--- pkg/daemon/controller/subnet.go
+++ pkg/daemon/controller/subnet.go
@@ -20,8 +20,19 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"reflect"
 
-	"github.com/alibaba/hybridnet/pkg/daemon/utils"
+	daemonutils "github.com/alibaba/hybridnet/pkg/daemon/utils"
+
+	corev1 "k8s.io/api/core/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	"github.com/alibaba/hybridnet/pkg/utils"
 
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
@@ -84,13 +95,13 @@ func (r *subnetReconciler) Reconcile(ctx context.Context, request reconcile.Requ
 		switch networkMode {
 		case networkingv1.NetworkModeVlan:
 			if isUnderlayOnHost {
-				forwardNodeIfName, err = utils.EnsureVlanIf(r.ctrlHubRef.config.NodeVlanIfName, netID)
+				forwardNodeIfName, err = daemonutils.EnsureVlanIf(r.ctrlHubRef.config.NodeVlanIfName, netID)
 				if err != nil {
 					return reconcile.Result{Requeue: true}, fmt.Errorf("failed to ensure vlan forward node interface: %v", err)
 				}
 			}
 		case networkingv1.NetworkModeVxlan:
-			forwardNodeIfName, err = utils.GenerateVxlanNetIfName(r.ctrlHubRef.config.NodeVxlanIfName, netID)
+			forwardNodeIfName, err = daemonutils.GenerateVxlanNetIfName(r.ctrlHubRef.config.NodeVxlanIfName, netID)
 			if err != nil {
 				return reconcile.Result{Requeue: true}, fmt.Errorf("failed to generate vxlan forward node if name: %v", err)
 			}
@@ -167,7 +178,7 @@ func (r *subnetReconciler) Reconcile(ctx context.Context, request reconcile.Requ
 		return reconcile.Result{Requeue: true}, fmt.Errorf("failed to sync ipv4 routes: %v", err)
 	}
 
-	globalDisabled, err := utils.CheckIPv6GlobalDisabled()
+	globalDisabled, err := daemonutils.CheckIPv6GlobalDisabled()
 	if err != nil {
 		return reconcile.Result{Requeue: true}, fmt.Errorf("failed to check ipv6 global disabled: %v", err)
 	}
@@ -185,4 +196,107 @@ func (r *subnetReconciler) Reconcile(ctx context.Context, request reconcile.Requ
 	r.ctrlHubRef.iptablesSyncTrigger()
 
 	return reconcile.Result{}, nil
+}
+
+func (r *subnetReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	subnetController, err := controller.New("subnet", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return fmt.Errorf("failed to create subnet controller: %v", err)
+	}
+
+	if err := subnetController.Watch(&source.Kind{Type: &networkingv1.Subnet{}},
+		&fixedKeyHandler{key: ActionReconcileSubnet},
+		&predicate.ResourceVersionChangedPredicate{},
+		&predicate.Funcs{
+			UpdateFunc: func(updateEvent event.UpdateEvent) bool {
+				oldSubnet := updateEvent.ObjectOld.(*networkingv1.Subnet)
+				newSubnet := updateEvent.ObjectNew.(*networkingv1.Subnet)
+
+				oldSubnetNetID := oldSubnet.Spec.NetID
+				newSubnetNetID := newSubnet.Spec.NetID
+
+				if (oldSubnetNetID == nil && newSubnetNetID != nil) ||
+					(oldSubnetNetID != nil && newSubnetNetID == nil) ||
+					(oldSubnetNetID != nil && newSubnetNetID != nil && *oldSubnetNetID != *newSubnetNetID) ||
+					oldSubnet.Spec.Network != newSubnet.Spec.Network ||
+					!reflect.DeepEqual(oldSubnet.Spec.Range, newSubnet.Spec.Range) ||
+					networkingv1.IsSubnetAutoNatOutgoing(&oldSubnet.Spec) != networkingv1.IsSubnetAutoNatOutgoing(&newSubnet.Spec) {
+					return true
+				}
+				return false
+			},
+		}); err != nil {
+		return fmt.Errorf("failed to watch networkingv1.Subnet for subnet controller: %v", err)
+	}
+
+	if err := subnetController.Watch(&source.Kind{Type: &networkingv1.Network{}},
+		&fixedKeyHandler{key: ActionReconcileSubnet},
+		&predicate.Funcs{
+			UpdateFunc: func(updateEvent event.UpdateEvent) bool {
+				oldNetwork := updateEvent.ObjectOld.(*networkingv1.Network)
+				newNetwork := updateEvent.ObjectNew.(*networkingv1.Network)
+
+				if utils.DeepEqualStringSlice(oldNetwork.Status.SubnetList, newNetwork.Status.SubnetList) ||
+					utils.DeepEqualStringSlice(oldNetwork.Status.NodeList, newNetwork.Status.NodeList) {
+					return true
+				}
+
+				if !reflect.DeepEqual(oldNetwork.Spec.Config, newNetwork.Spec.Config) {
+					return true
+				}
+
+				return false
+			},
+		},
+	); err != nil {
+		return fmt.Errorf("failed to watch networkingv1.Network for subnet controller: %v", err)
+	}
+
+	if err := subnetController.Watch(&source.Kind{Type: &corev1.Node{}},
+		&fixedKeyHandler{key: ActionReconcileSubnet},
+		predicate.Funcs{
+			CreateFunc: func(createEvent event.CreateEvent) bool {
+				return false
+			},
+			UpdateFunc: func(updateEvent event.UpdateEvent) bool {
+				return checkNodeUpdate(updateEvent)
+			},
+			DeleteFunc: func(deleteEvent event.DeleteEvent) bool {
+				return false
+			},
+			GenericFunc: func(genericEvent event.GenericEvent) bool {
+				return false
+			},
+		}); err != nil {
+		return fmt.Errorf("failed to watch corev1.Node for subnet controller: %v", err)
+	}
+
+	if err := subnetController.Watch(r.ctrlHubRef.subnetControllerTriggerSource, &handler.Funcs{}); err != nil {
+		return fmt.Errorf("failed to watch subnetControllerTriggerSource for subnet controller: %v", err)
+	}
+
+	// enable multicluster feature
+	if feature.MultiClusterEnabled() {
+		if err := subnetController.Watch(&source.Kind{
+			Type: &multiclusterv1.RemoteSubnet{}},
+			&fixedKeyHandler{key: ActionReconcileSubnet},
+			predicate.Funcs{
+				UpdateFunc: func(updateEvent event.UpdateEvent) bool {
+					oldRs := updateEvent.ObjectOld.(*multiclusterv1.RemoteSubnet)
+					newRs := updateEvent.ObjectNew.(*multiclusterv1.RemoteSubnet)
+
+					if oldRs.Spec.ClusterName != newRs.Spec.ClusterName ||
+						!reflect.DeepEqual(oldRs.Spec.Range, newRs.Spec.Range) ||
+						multiclusterv1.GetRemoteSubnetType(oldRs) != multiclusterv1.GetRemoteSubnetType(newRs) {
+						return true
+					}
+					return false
+				},
+			},
+		); err != nil {
+			return fmt.Errorf("failed to watch multiclusterv1.RemoteSubnet for subnet controller: %v", err)
+		}
+	}
+
+	return nil
 }
