f127e92504b37f953defead87b84dbe2ec7b49a4
Bruce Ma <brucema19901024@gmail.com>
2022-07-13 09:28:14 +0800

    integration test: check node attachment label dynamic update when changing network selecotr, node label and global unique network
    Signed-off-by: Bruce Ma <brucema19901024@gmail.com>

--- pkg/controllers/networking/node_controller_test.go
+++ pkg/controllers/networking/node_controller_test.go
@@ -18,13 +18,18 @@ package networking_test
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/uuid"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
+	networkingv1 "github.com/alibaba/hybridnet/pkg/apis/networking/v1"
 	"github.com/alibaba/hybridnet/pkg/constants"
 )
 
@@ -88,11 +93,356 @@ var _ = Describe("Node controller integration test suite", func() {
 					g.Expect(node.Labels).NotTo(HaveKey(constants.LabelUnderlayNetworkAttachment))
 					g.Expect(node.Labels).To(HaveKey(constants.LabelOverlayNetworkAttachment))
 					g.Expect(node.Labels).NotTo(HaveKey(constants.LabelBGPNetworkAttachment))
-
 				}).
 				WithTimeout(30 * time.Second).
 				WithPolling(time.Second).
 				Should(Succeed())
+		})
+	})
+
+	Context("Dynamic update check", func() {
+		It("Check node attachment label update after changing network selector", func() {
+			By("create test underlay network")
+			networkName := fmt.Sprintf("test-underlay-network-%s", uuid.NewUUID())
+			anotherNetworkName := fmt.Sprintf("test-underlay-network-%s", uuid.NewUUID())
+			network := underlayNetworkRender(networkName, 200)
+			Expect(k8sClient.Create(context.Background(), network)).NotTo(HaveOccurred())
+
+			By("create test nodes")
+			node1Name := fmt.Sprintf("test-node-%s", uuid.NewUUID())
+			node2Name := fmt.Sprintf("test-node-%s", uuid.NewUUID())
+			node1 := nodeRender(node1Name, map[string]string{
+				"network": networkName,
+			})
+			node2 := nodeRender(node2Name, map[string]string{
+				"network": anotherNetworkName,
+			})
+			Expect(k8sClient.Create(context.Background(), node1)).NotTo(HaveOccurred())
+			Expect(k8sClient.Create(context.Background(), node2)).NotTo(HaveOccurred())
+
+			By("checking node underlay attachment label")
+			Eventually(
+				func(g Gomega) {
+					currentNode1 := &corev1.Node{}
+					g.Expect(k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name: node1Name,
+						},
+						currentNode1)).NotTo(HaveOccurred())
+
+					g.Expect(currentNode1.Labels).To(HaveKey(constants.LabelUnderlayNetworkAttachment))
+
+					currentNode2 := &corev1.Node{}
+					g.Expect(k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name: node2Name,
+						},
+						currentNode2)).NotTo(HaveOccurred())
+
+					g.Expect(currentNode2.Labels).NotTo(HaveKey(constants.LabelUnderlayNetworkAttachment))
+				}).
+				WithTimeout(30 * time.Second).
+				WithPolling(time.Second).
+				Should(Succeed())
+
+			By("update underlay network selector to another name")
+			Expect(
+				func() error {
+					tempNetwork := &networkingv1.Network{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: networkName,
+						},
+					}
+					_, err := controllerutil.CreateOrPatch(context.Background(), k8sClient, tempNetwork, func() error {
+						tempNetwork.Spec.NodeSelector = map[string]string{
+							"network": anotherNetworkName,
+						}
+						return nil
+					})
+					return err
+				}(),
+			).NotTo(HaveOccurred())
+
+			By("checking node underlay attachment label update")
+			Eventually(
+				func(g Gomega) {
+					currentNode1 := &corev1.Node{}
+					g.Expect(k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name: node1Name,
+						},
+						currentNode1)).NotTo(HaveOccurred())
+
+					g.Expect(currentNode1.Labels).NotTo(HaveKey(constants.LabelUnderlayNetworkAttachment))
+
+					currentNode2 := &corev1.Node{}
+					g.Expect(k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name: node2Name,
+						},
+						currentNode2)).NotTo(HaveOccurred())
+
+					g.Expect(currentNode2.Labels).To(HaveKey(constants.LabelUnderlayNetworkAttachment))
+				}).
+				WithTimeout(30 * time.Second).
+				WithPolling(time.Second).
+				Should(Succeed())
+
+			By("rollback underlay network selector to its own name")
+			Expect(
+				func() error {
+					tempNetwork := &networkingv1.Network{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: networkName,
+						},
+					}
+					_, err := controllerutil.CreateOrPatch(context.Background(), k8sClient, tempNetwork, func() error {
+						tempNetwork.Spec.NodeSelector = map[string]string{
+							"network": networkName,
+						}
+						return nil
+					})
+					return err
+				}(),
+			).NotTo(HaveOccurred())
+
+			By("checking node underlay attachment label update again")
+			Eventually(
+				func(g Gomega) {
+					currentNode1 := &corev1.Node{}
+					g.Expect(k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name: node1Name,
+						},
+						currentNode1)).NotTo(HaveOccurred())
+
+					g.Expect(currentNode1.Labels).To(HaveKey(constants.LabelUnderlayNetworkAttachment))
+
+					currentNode2 := &corev1.Node{}
+					g.Expect(k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name: node2Name,
+						},
+						currentNode2)).NotTo(HaveOccurred())
+
+					g.Expect(currentNode2.Labels).NotTo(HaveKey(constants.LabelUnderlayNetworkAttachment))
+				}).
+				WithTimeout(30 * time.Second).
+				WithPolling(time.Second).
+				Should(Succeed())
+
+			By("remove the test objects")
+			Expect(k8sClient.Delete(context.Background(), network)).NotTo(HaveOccurred())
+			Expect(k8sClient.Delete(context.Background(), node1)).NotTo(HaveOccurred())
+			Expect(k8sClient.Delete(context.Background(), node2)).NotTo(HaveOccurred())
+		})
+
+		It("Check node attachment label update after changing node related label", func() {
+			By("create test underlay network")
+			networkName := fmt.Sprintf("test-underlay-network-%s", uuid.NewUUID())
+			network := underlayNetworkRender(networkName, 200)
+			Expect(k8sClient.Create(context.Background(), network)).NotTo(HaveOccurred())
+
+			By("create test node")
+			nodeName := fmt.Sprintf("test-node-%s", uuid.NewUUID())
+			node := nodeRender(nodeName, map[string]string{
+				"network": networkName,
+			})
+			Expect(k8sClient.Create(context.Background(), node)).NotTo(HaveOccurred())
+
+			By("checking node underlay attachment label")
+			Eventually(
+				func(g Gomega) {
+					currentNode := &corev1.Node{}
+					g.Expect(k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name: nodeName,
+						},
+						currentNode)).NotTo(HaveOccurred())
+
+					g.Expect(currentNode.Labels).To(HaveKey(constants.LabelUnderlayNetworkAttachment))
+				}).
+				WithTimeout(30 * time.Second).
+				WithPolling(time.Second).
+				Should(Succeed())
+
+			By("update node label about network selector")
+			Expect(
+				func() error {
+					tempNode := &corev1.Node{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: nodeName,
+						},
+					}
+					_, err := controllerutil.CreateOrPatch(context.Background(), k8sClient, tempNode, func() error {
+						tempNode.Labels["network"] = "unexpected"
+						return nil
+					})
+					return err
+				}(),
+			).NotTo(HaveOccurred())
+
+			By("checking node underlay attachment label update")
+			Eventually(
+				func(g Gomega) {
+					currentNode := &corev1.Node{}
+					g.Expect(k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name: nodeName,
+						},
+						currentNode)).NotTo(HaveOccurred())
+
+					g.Expect(currentNode.Labels).NotTo(HaveKey(constants.LabelUnderlayNetworkAttachment))
+				}).
+				WithTimeout(30 * time.Second).
+				WithPolling(time.Second).
+				Should(Succeed())
+
+			By("rollback network selector label on node")
+			Expect(
+				func() error {
+					tempNode := &corev1.Node{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: nodeName,
+						},
+					}
+					_, err := controllerutil.CreateOrPatch(context.Background(), k8sClient, tempNode, func() error {
+						tempNode.Labels["network"] = networkName
+						return nil
+					})
+					return err
+				}(),
+			).NotTo(HaveOccurred())
+
+			By("checking node underlay attachment label update again")
+			Eventually(
+				func(g Gomega) {
+					currentNode := &corev1.Node{}
+					g.Expect(k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name: nodeName,
+						},
+						currentNode)).NotTo(HaveOccurred())
+
+					g.Expect(currentNode.Labels).To(HaveKey(constants.LabelUnderlayNetworkAttachment))
+				}).
+				WithTimeout(30 * time.Second).
+				WithPolling(time.Second).
+				Should(Succeed())
+
+			By("remove the test objects")
+			Expect(k8sClient.Delete(context.Background(), network)).NotTo(HaveOccurred())
+			Expect(k8sClient.Delete(context.Background(), node)).NotTo(HaveOccurred())
+		})
+
+		It("Check node attachment label update after creating globalBGP network", func() {
+			By("create test underlay network of BGP mode")
+			networkName := fmt.Sprintf("test-underlay-network-%s", uuid.NewUUID())
+			network := underlayNetworkRender(networkName, 123)
+			network.Spec.Mode = networkingv1.NetworkModeBGP
+			Expect(k8sClient.Create(context.Background(), network)).NotTo(HaveOccurred())
+
+			By("create test node")
+			nodeName := fmt.Sprintf("test-node-%s", uuid.NewUUID())
+			node := nodeRender(nodeName, map[string]string{
+				"network": networkName,
+			})
+			Expect(k8sClient.Create(context.Background(), node)).NotTo(HaveOccurred())
+
+			By("checking node underlay and globalBGP attachment label")
+			Eventually(
+				func(g Gomega) {
+					currentNode := &corev1.Node{}
+					g.Expect(k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name: nodeName,
+						},
+						currentNode)).NotTo(HaveOccurred())
+
+					g.Expect(currentNode.Labels).To(HaveKey(constants.LabelUnderlayNetworkAttachment))
+					g.Expect(currentNode.Labels).NotTo(HaveKey(constants.LabelBGPNetworkAttachment))
+				}).
+				WithTimeout(30 * time.Second).
+				WithPolling(time.Second).
+				Should(Succeed())
+
+			By("create test globalBGP network")
+			globalBGPNetworkName := fmt.Sprintf("test-globalbgp-network-%s", uuid.NewUUID())
+			globalBGPNetwork := globalBGPNetworkRender(globalBGPNetworkName, 210)
+			Expect(k8sClient.Create(context.Background(), globalBGPNetwork)).NotTo(HaveOccurred())
+
+			By("checking node globalBGP attachment label updated")
+			Eventually(
+				func(g Gomega) {
+					currentNode := &corev1.Node{}
+					g.Expect(k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name: nodeName,
+						},
+						currentNode)).NotTo(HaveOccurred())
+
+					g.Expect(currentNode.Labels).To(HaveKey(constants.LabelUnderlayNetworkAttachment))
+					g.Expect(currentNode.Labels).To(HaveKey(constants.LabelBGPNetworkAttachment))
+				}).
+				WithTimeout(30 * time.Second).
+				WithPolling(time.Second).
+				Should(Succeed())
+
+			By("remove test globalBGP network")
+			Expect(k8sClient.Delete(context.Background(), globalBGPNetwork)).NotTo(HaveOccurred())
+
+			By("checking node globalBGP attachment label removed")
+			Eventually(
+				func(g Gomega) {
+					currentNode := &corev1.Node{}
+					g.Expect(k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name: nodeName,
+						},
+						currentNode)).NotTo(HaveOccurred())
+
+					g.Expect(currentNode.Labels).To(HaveKey(constants.LabelUnderlayNetworkAttachment))
+					g.Expect(currentNode.Labels).NotTo(HaveKey(constants.LabelBGPNetworkAttachment))
+				}).
+				WithTimeout(30 * time.Second).
+				WithPolling(time.Second).
+				Should(Succeed())
+
+			By("remove test underlay network")
+			Expect(k8sClient.Delete(context.Background(), network)).NotTo(HaveOccurred())
+
+			By("checking node underlay and globalBGP attachment label removed")
+			Eventually(
+				func(g Gomega) {
+					currentNode := &corev1.Node{}
+					g.Expect(k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name: nodeName,
+						},
+						currentNode)).NotTo(HaveOccurred())
+
+					g.Expect(currentNode.Labels).NotTo(HaveKey(constants.LabelUnderlayNetworkAttachment))
+					g.Expect(currentNode.Labels).NotTo(HaveKey(constants.LabelBGPNetworkAttachment))
+				}).
+				WithTimeout(30 * time.Second).
+				WithPolling(time.Second).
+				Should(Succeed())
+
+			By("remove the test node")
+			Expect(k8sClient.Delete(context.Background(), node)).NotTo(HaveOccurred())
 		})
 	})
 
