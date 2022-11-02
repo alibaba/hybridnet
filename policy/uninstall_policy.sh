44488a9f101ab66ebd582153b4ca9e70ee8dc301
Bruce Ma <brucema19901024@gmail.com>
2022-11-02 20:23:02 +0800

    Merge pull request #330 from hhyasdf/uninstall-policy-script-for-ipv6
    update uninstall_policy.sh for ipv6

--- policy/uninstall_policy.sh
+++ policy/uninstall_policy.sh
@@ -34,6 +34,30 @@ cleanup_felix() {
 
     echo "Cleaning up calico rules from the filter table..."
     iptables-save -t filter | grep -e '--comment "cali:' | cut -c 3- | sed 's/^ *//;s/ *$//' | xargs -l1 iptables -t filter -D
+
+    echo "Flushing all the calico ip6tables chains in the nat table..."
+    ip6tables-save -t nat | grep -oP '(?<!^:)cali-[^ ]+' | while read line; do ip6tables -t nat -F "$line"; done
+
+    echo "Flushing all the calico ip6tables chains in the raw table..."
+    ip6tables-save -t raw | grep -oP '(?<!^:)cali-[^ ]+' | while read line; do ip6tables -t raw -F "$line"; done
+
+    echo "Flushing all the calico ip6tables chains in the mangle table..."
+    ip6tables-save -t mangle | grep -oP '(?<!^:)cali-[^ ]+' | while read line; do ip6tables -t mangle -F "$line"; done
+
+    echo "Flushing all the calico ip6tables chains in the filter table..."
+    ip6tables-save -t filter | grep -oP '(?<!^:)cali-[^ ]+' | while read line; do ip6tables -t filter -F "$line"; done
+
+    echo "Cleaning up calico rules from the nat table..."
+    ip6tables-save -t nat | grep -e '--comment "cali:' | cut -c 3- | sed 's/^ *//;s/ *$//' | xargs -l1 ip6tables -t nat -D
+
+    echo "Cleaning up calico rules from the raw table..."
+    ip6tables-save -t raw | grep -e '--comment "cali:' | cut -c 3- | sed 's/^ *//;s/ *$//' | xargs -l1 ip6tables -t raw -D
+
+    echo "Cleaning up calico rules from the mangle table..."
+    ip6tables-save -t mangle | grep -e '--comment "cali:' | cut -c 3- | sed 's/^ *//;s/ *$//' | xargs -l1 ip6tables -t mangle -D
+
+    echo "Cleaning up calico rules from the filter table..."
+    ip6tables-save -t filter | grep -e '--comment "cali:' | cut -c 3- | sed 's/^ *//;s/ *$//' | xargs -l1 ip6tables -t filter -D
 }
 
 cleanup_felix
